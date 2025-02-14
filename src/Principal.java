039aca50531b09726113f05487358cfcff9c47b8
Marilaine Medeiros <marilainemedeiros12@gmail.com>
2025-02-13 22:38:48 -0300

    exercicio2

--- src/Principal.java
+++ src/Principal.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class Principal {
+    public static void main(String[] args) {
+        int n = 15;
+        int soma = (n * (n + 1)) / 2;
+/*
+Utilizei a matemática pura
+15 x (15+1) / 2
+15 x 16 / 2
+240 / 2 = 120
+ */
+        System.out.println("A soma dos números de 1 a 15 é: " + soma);
+    }
 }
